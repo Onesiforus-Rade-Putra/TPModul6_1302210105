be497bedf67263f4794ed2d78f4817267a6a2304
Onesiforus-Rade-Putra <onesiforusradeputra@gmail.com>
2023-03-25 15:27:34 +0700

    menambahkan class SayaTubeVideo

---
+++ TPModul6_1302210105/SayaTubeVideo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPModul6_1302210105
+{
+    internal class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string title)
+        {
+            Random rnd = new Random();
+            this.title = title;
+            this.playCount = 0;
+            this.id = rnd.Next(100000);
+        }
+        public void IncreasePlayCount(int count)
+        {
+            this.playCount = count;
+        }
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("Video memiliki judul: " + title);
+            Console.WriteLine("Video memiliki ID: " + id);
+            Console.WriteLine("Video diputar sebanyak: " + playCount);
+        }
+    }
+}
